68557d9a78356aec2f59133b94edadd0e8b72e74
Luiz Felipe Mendes <felipe14_mendes@hotmail.com>
2016-12-27 17:01:16 -0200

    +2

---
+++ ex8_calcSal.py
@@ -0,0 +1,6 @@
+salarioHora = input('Entre com o seu salario por hora: ')
+horasMes = input('Entre com as suas horas trabalhadas: ')
+
+salario = salarioHora * horasMes
+
+print ("Seu salario nesse mes foi de: "),salario
---
+++ ex9_convertFtoC.py
@@ -0,0 +1,4 @@
+F = input("Entre com a temp em ºF")
+C = (5*(F-32))/9
+
+print "Valor em C ",C
